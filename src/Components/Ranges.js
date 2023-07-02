ad68af95d3072aaa8c76be38961ace03ffa48c5e
Keshav-mittal <aggarwalkeshav001@gmail.com>
2023-07-02 12:33:20 +0530

    make some changes in ranges of skills

--- src/Components/Ranges.js
+++ src/Components/Ranges.js
@@ -8,36 +8,36 @@ const Ranges = () => {
                 <div className={styles.dvi}>
                     <h3 className={styles.h3style} >JavaScript</h3>
                     <div className={styles.dv}>
-                        <div className={styles.dvinner}></div>
-                        <div className={styles.dvtwoinner}></div>
+                        <div className={styles.dvinnerjs}></div>
+                        <div className={styles.dvtwoinnerjs}></div>
                     </div>
                 </div>
                 <div className={styles.dvi}>
                     <h3 className={styles.h3style} >Cpp</h3>
                     <div className={styles.dv}>
-                        <div className={styles.dvinner}></div>
-                        <div className={styles.dvtwoinner}></div>
+                        <div className={styles.dvinnercpp}></div>
+                        <div className={styles.dvtwoinnercpp}></div>
                     </div>
                 </div>
                 <div className={styles.dvi}>
                     <h3 className={styles.h3style} >HTML</h3>
                     <div className={styles.dv}>
-                        <div className={styles.dvinner}></div>
-                        <div className={styles.dvtwoinner}></div>
+                        <div className={styles.dvinnerhtml}></div>
+                        <div className={styles.dvtwoinnerhtml}></div>
                     </div>
                 </div>
                 <div className={styles.dvi}>
                     <h3 className={styles.h3style} >CSS</h3>
                     <div className={styles.dv}>
-                        <div className={styles.dvinner}></div>
-                        <div className={styles.dvtwoinner}></div>
+                        <div className={styles.dvinnercss}></div>
+                        <div className={styles.dvtwoinnercss}></div>
                     </div>
                 </div>
                 <div className={styles.dvi}>
                     <h3 className={styles.h3style} >Reactjs</h3>
                     <div className={styles.dv}>
-                        <div className={styles.dvinner}></div>
-                        <div className={styles.dvtwoinner}></div>
+                        <div className={styles.dvinnerreact}></div>
+                        <div className={styles.dvtwoinnerreact}></div>
                     </div>
                 </div>
             </div>
